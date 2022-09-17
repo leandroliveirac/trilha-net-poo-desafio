0c5b555f82e2395182b65b0f775be08310dff54a
leandroliveirac <leandroparipe@hotmail.com>
2022-09-17 11:09:06 -0300

    Adicionando algoritmo para teste do app

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero:"199999999",modelo:"modelo 1",emei:"99999999",memoria:32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero:"188888888",modelo:"modelo 2",emei:"88888888",memoria:64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
