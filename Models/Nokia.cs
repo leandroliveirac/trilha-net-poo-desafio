684b7e5effebc62e583ae43c010029416976fa67
leandroliveirac <leandroparipe@hotmail.com>
2022-09-17 11:04:26 -0300

    Adicionado os recursos na classe Nokia e Iphone

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,13 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string emei, int memoria) : base(numero, modelo, emei, memoria)
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Nokia");
+        }
     }
 }
